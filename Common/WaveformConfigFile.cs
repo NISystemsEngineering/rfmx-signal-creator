4a49a0b0d5ef758a23a7b4e8036bfaa9efea42ff
Michael Whitten <michael.whitten@ni.com>
2020-07-21 08:46:09 -0500

    Added RFmx instr configuration saving to waveform file type class

--- Common/WaveformConfigFile.cs
+++ Common/WaveformConfigFile.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using NationalInstruments.RFmx.InstrMX;
 
 namespace NationalInstruments.Utilities.WaveformParsing
 {
@@ -24,15 +25,43 @@ namespace NationalInstruments.Utilities.WaveformParsing
                     return null;
             }
         }
+
+        public abstract void SaveConfiguration(RFmxInstrMX instr);
+        public abstract void SaveConfiguration(RFmxInstrMX instr, string outputDirectory);
     }
     public class TdmsFile : WaveformConfigFile
     {
         internal TdmsFile(string filePath)
             : base(filePath) { }
+
+        public override void SaveConfiguration(RFmxInstrMX instr)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SaveConfiguration(RFmxInstrMX instr, string outputDirectory)
+        {
+            throw new System.NotImplementedException();
+        }
     }
     public class RfwsFile : WaveformConfigFile
     {
         internal RfwsFile(string filePath)
             : base(filePath) { }
+
+        public override void SaveConfiguration(RFmxInstrMX instr)
+        {
+            string directory = Path.GetDirectoryName(FilePath);
+            SaveConfiguration(instr, directory);
+        }
+
+        public override void SaveConfiguration(RFmxInstrMX instr, string outputDirectory)
+        {
+            string newFileName = "RFmx_Config_" + Path.GetFileNameWithoutExtension(FilePath) + ".tdms";
+
+            string newPath = Path.Combine(outputDirectory, newFileName);
+
+            instr.SaveAllConfigurations(newPath);
+        }
     }
 }
