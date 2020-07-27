f483277b0ff114515aebd28e3d984fe290a3917a
Michael Whitten <michael.whitten@ni.com>
2020-07-27 11:57:24 -0500

    Prepared release build output

--- Source/RFmxSignalCreator/Properties/AssemblyInfo.cs
+++ Source/RFmxSignalCreator/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RfGeneratorToAnalyzer")]
+[assembly: AssemblyTitle("RFmxSignalCreator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RfGeneratorToAnalyzer")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("National Instruments")]
+[assembly: AssemblyProduct("RFmxSignalCreator")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
