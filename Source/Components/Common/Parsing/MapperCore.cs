be0381a8c1c2fabf026b82a190eefad2a04d9118
Michael Whitten <michael.whitten@ni.com>
2020-08-07 15:48:54 -0500

    Migrated generic parser code to common project

---
+++ Source/Components/Common/Parsing/MapperCore.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Serilog;
+using Serilog.Context;
+using System.Reflection;
+using NationalInstruments.RFmx.InstrMX;
+
+namespace NationalInstruments.Utilities.WaveformParsing
+{
+    /// <summary>
+    /// Maps settings to the appropriate RFmx properties.
+    /// </summary>
+    /// <typeparam name="T">Specifies the RFmx signal type to be configured</typeparam>
+    public abstract class MapperCore<T> where T : ISignalConfiguration
+    {
+        public T Signal { get; }
+
+        public MapperCore(T signal)
+        {
+            Signal = signal;
+        }
+
+        #region Core Map Functions
+        /// <summary>
+        /// Represents a mapping operation to translate from an <see cref="RFmxPropertyGroup{T}"/> object to the appropriate RFmx
+        /// settings as defined by <see cref="RFmxPropertyMap{T}"/>. 
+        /// </summary>
+        public virtual void Map(ParsingGroup group)
+        {
+            using (LogContext.PushProperty("Group", group.GetType().Name))
+            {
+                try
+                {
+                    group.CustomConfigure(Signal);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, "Exception thrown performing custom configuration for {Group}", group.GetType().Name);
+                    return;
+                }
+
+                // Discard the attribute; we only need the keys and associated values
+                foreach ((FieldInfo field, object property) in group.MappedFields)
+                {
+                    using (LogContext.PushProperty("Field", field.Name))
+                    {
+                        try
+                        {
+                            // Invoke the correct instance of the Apply Configuration overloaded method based on the type of the generic
+                            // parameter. Skip all keys that do not have a value (meaning they were not found or version was not a match).
+                            ApplyConfigurationCore(group, property);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(ex, "Exception occured applying property {Property}", property);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ApplyConfigurationCore(ParsingGroup group, object property)
+        {
+            switch (property)
+            {
+                case RFmxPropertyMap<bool> boolKey:
+                    if (boolKey.HasValue)
+                    {
+                        ApplyConfiguration(group.SelectorString, boolKey);
+                    }
+                    break;
+                case RFmxPropertyMap<double> doubleKey:
+                    if (doubleKey.HasValue)
+                    {
+                        ApplyConfiguration(group.SelectorString, doubleKey);
+                    }
+                    break;
+                case RFmxPropertyMap<int> intKey:
+                    if (intKey.HasValue)
+                    {
+                        ApplyConfiguration(group.SelectorString, intKey);
+                    }
+                    break;
+                case RFmxPropertyMap<string> stringKey:
+                    if (stringKey.HasValue)
+                    {
+                        ApplyConfiguration(group.SelectorString, stringKey);
+                    }
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+        #endregion
+
+        #region Abstract Methods
+        /// <summary>
+        /// Applies a setting defined by <paramref name="key"/> to the RFmx signal and selector string 
+        /// defined in <paramref name="section"/>.
+        /// </summary>
+        /// <param name="section">Specifies the section, containing the RFmx signal and selector string, to apply the setting to.</param>
+        /// <param name="key">Specifies the RFmx parameter ID and value to be set.</param>
+        protected abstract void ApplyConfiguration(string selectorString, RFmxPropertyMap<bool> key);
+        /// <summary>
+        /// Applies a setting defined by <paramref name="key"/> to the RFmx signal and selector string 
+        /// defined in <paramref name="section"/>.
+        /// </summary>
+        /// <param name="section">Specifies the section, containing the RFmx signal and selector string, to apply the setting to.</param>
+        /// <param name="key">Specifies the RFmx parameter ID and value to be set.</param>
+        protected abstract void ApplyConfiguration(string selectorString, RFmxPropertyMap<double> key);
+        /// <summary>
+        /// Applies a setting defined by <paramref name="key"/> to the RFmx signal and selector string 
+        /// defined in <paramref name="section"/>.
+        /// </summary>
+        /// <param name="section">Specifies the section, containing the RFmx signal and selector string, to apply the setting to.</param>
+        /// <param name="key">Specifies the RFmx parameter ID and value to be set.</param>
+        protected abstract void ApplyConfiguration(string selectorString, RFmxPropertyMap<int> key);
+        /// <summary>
+        /// Applies a setting defined by <paramref name="key"/> to the RFmx signal and selector string 
+        /// defined in <paramref name="section"/>.
+        /// </summary>
+        /// <param name="section">Specifies the section, containing the RFmx signal and selector string, to apply the setting to.</param>
+        /// <param name="key">Specifies the RFmx parameter ID and value to be set.</param>
+        protected abstract void ApplyConfiguration(string selectorString, RFmxPropertyMap<string> key);
+        #endregion
+
+    }
+}
---
+++ Source/Components/Common/Parsing/ParserCore.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using Serilog;
+using Serilog.Context;
+
+namespace NationalInstruments.Utilities.WaveformParsing
+{
+    public abstract class ParserCore
+    {
+        #region Parse Functions
+        public virtual void Parse(ParsingGroup group)
+        {
+            using (LogContext.PushProperty("Group", group.GetType().Name))
+            {
+                foreach (var field in group.MappedFields)
+                {
+                    try
+                    {
+                        if (ValidateProperty(group, field))
+                        {
+                            try
+                            {
+                                object value = FetchValue(group, field);
+                                Log.Verbose("Read property {Property} with value {Value}", field.Field.Name, value);
+                                try
+                                {
+                                    ParseAndApplyValue(field, value);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Log.Error(ex, "Error parsing property {Property}; attempted to parse {Value} but operation failed",
+                                        field.Field.Name, value);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Log.Error(ex, "Failed to fetch value for {Property}", field.Field.Name);
+                            }
+                        }
+                        else
+                        {
+                            LogFailedPropertyValidation(group, field);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error(ex, "Error validating property {Property}", field.Field.Name);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        private void ParseAndApplyValue(FieldValuePair field, object value)
+        {
+            switch (field.Value)
+            {
+                case RFmxPropertyMap<bool> boolKey:
+                    // If delegate is not set, then just directly parse the value
+                    if (boolKey.CustomMap == null)
+                        boolKey.Value = ParseValue<bool>(value);
+                    // Otherwise, invoke the delgate to manually map the value
+                    else
+                        boolKey.Value = boolKey.CustomMap(value);
+                    break;
+                case RFmxPropertyMap<double> doubleKey:
+                    // If delegate is not set, then just directly parse the value
+                    if (doubleKey.CustomMap == null)
+                        doubleKey.Value = ParseValue<double>(value);
+                    // Otherwise, invoke the delgate to manually map the value
+                    else
+                        doubleKey.Value = doubleKey.CustomMap(value);
+                    break;
+                case RFmxPropertyMap<int> intKey:
+                    // If delegate is not set, then just directly parse the value
+                    if (intKey.CustomMap == null)
+                        intKey.Value = ParseValue<int>(value);
+                    // Otherwise, invoke the delgate to manually map the value
+                    else
+                        intKey.Value = intKey.CustomMap(value);
+                    break;
+                case RFmxPropertyMap<string> stringKey:
+                    // If delegate is not set, then just directly pass the value
+                    if (stringKey.CustomMap == null)
+                        stringKey.Value = ParseValue<string>(value);
+                    // Otherwise, invoke the delgate to manually map the value
+                    else
+                        stringKey.Value = stringKey.CustomMap(value);
+                    break;
+                default:
+                    throw new NotImplementedException($"Key type {field.Value.GetType()} not supported");
+            }
+        }
+
+        protected virtual bool ValidateProperty(ParsingGroup group, FieldValuePair field) => true;
+        protected abstract object FetchValue(ParsingGroup group, FieldValuePair field);
+        protected virtual T ParseValue<T>(object value) => (T)value;
+        #endregion
+
+
+        #region Optional Log Functions
+        protected virtual void LogFailedPropertyValidation(ParsingGroup group, FieldValuePair field)
+        {
+            Log.Debug("{KeyName} skipped because it did not pass the validation check.", field.Field.Name);
+        }
+        #endregion
+    }
+}
