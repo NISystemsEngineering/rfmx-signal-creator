2b466edd734dddf83d44741054e895fd2b983443
Michael Whitten <michael.whitten@ni.com>
2020-08-20 10:35:48 -0500

    Added new unit tests to validate RB allocation

---
+++ Tests/SignalCreatorTests/NrPluginTests/NrPluginTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NationalInstruments.Utilities.SignalCreator.Plugins;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace NrPluginTests
+{
+    [TestClass]
+    public class NrPluginTests
+    {
+        [TestMethod]
+        public void ParseRbAllocationStringTests()
+        {
+            List<(int,int)> MkList(params (int,int)[] items)
+            {
+                return new List<(int, int)>(items);
+            }
+
+            var testCases = new Dictionary<string, List<(int, int)>>
+            {
+                ["0:last"] = MkList((0,-1)),
+                ["10:30"] = MkList((10,21)),
+                ["0,1,2,3,4,5"] = MkList((0,6)),
+                ["50:last,27:30,0,1,2,5,9"] = MkList((0,3),(5,1),(9,1),(27,4),(50,-1))
+            };
+            
+            foreach (var pair in testCases)
+            {
+                var result = NrParsingUtilities.ParseRbAllocationString(pair.Key);
+                result.Should().ContainInOrder(pair.Value);
+            }
+        }
+    }
+}
---
+++ Tests/SignalCreatorTests/NrPluginTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NrPluginTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NrPluginTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c59d754-7893-4a54-95ff-76db4cbeb984")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
