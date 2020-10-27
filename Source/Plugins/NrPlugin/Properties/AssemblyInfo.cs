568d7c5444d01c627d68689b8ab4d7d1751c7ee2
mwhitten <michael.whitten@ni.com>
2020-10-27 11:22:20 -0500

    Updated all versions to 2.0

--- Source/Plugins/NrPlugin/Properties/AssemblyInfo.cs
+++ Source/Plugins/NrPlugin/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NrPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A plugin that enables the Signal Creator for NI-RFmx to parse RFmx Waveform Creator files (.rfws) containing 5G New Radio waveform configurations.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NrPlugin")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
